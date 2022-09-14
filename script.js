bb31d001b882c6951851a53c9febbbc4559a61e7
Raihan <rhnchdri@gmail.com>
2022-09-15 02:29:37 +0600

    title update

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -124,7 +124,7 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+starts.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
